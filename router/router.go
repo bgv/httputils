6df2d7225ade0e71a34fab1a272217bf1a9566ce
Boris V <boris@codechimp.io>
2015-09-22 10:57:15 +0100

    Add httprouter routing util

---
+++ router/router.go
@@ -0,0 +1,119 @@
+package router
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/julienschmidt/httprouter"
+	"golang.org/x/net/context"
+)
+
+var (
+	mutex = sync.RWMutex{}
+	ctxts = map[*http.Request]context.Context{}
+)
+
+// Context will return the context for the request.
+func Context(r *http.Request) context.Context {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	return ctxts[r]
+}
+
+type param string
+
+// Param returns param p for the context.
+func Param(ctx context.Context, p string) string {
+	return ctx.Value(param(p)).(string)
+}
+
+// handle turns a Handle into httprouter.Handle
+func handle(handlerFunc http.HandlerFunc) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		for _, p := range params {
+			ctx = context.WithValue(ctx, param(p.Key), p.Value)
+		}
+
+		mutex.Lock()
+		ctxts[r] = ctx
+		mutex.Unlock()
+
+		handlerFunc(w, r)
+
+		mutex.Lock()
+		delete(ctxts, r)
+		mutex.Unlock()
+	}
+}
+
+// Router is wrapper around httprouter.Router with added support for prefixed sub-routes.
+type Router struct {
+	rtr    *httprouter.Router
+	prefix string
+}
+
+// New returns a new Router.
+func New() *Router {
+	return &Router{rtr: httprouter.New()}
+}
+
+// WithPrefix returns a router that prefixes all registered routes with prefix.
+func (r *Router) WithPrefix(prefix string) *Router {
+	return &Router{rtr: r.rtr, prefix: r.prefix + prefix}
+}
+
+// Map all supported httprouter methods
+
+// Delete registers a new DELETE route.
+func (r *Router) Delete(path string, h http.HandlerFunc) {
+	r.rtr.DELETE(r.prefix+path, handle(h))
+}
+
+// Get registers a new GET route.
+func (r *Router) Get(path string, h http.HandlerFunc) {
+	r.rtr.GET(r.prefix+path, handle(h))
+}
+
+// Head registers a new HEAD route.
+func (r *Router) Head(path string, h http.HandlerFunc) {
+	r.rtr.HEAD(r.prefix+path, handle(h))
+}
+
+// Options registers a new OPTIONS route.
+func (r *Router) Options(path string, h http.HandlerFunc) {
+	r.rtr.OPTIONS(r.prefix+path, handle(h))
+}
+
+// Patch registers a new PATCH route.
+func (r *Router) Patch(path string, h http.HandlerFunc) {
+	r.rtr.PATCH(r.prefix+path, handle(h))
+}
+
+// Post registers a new POST route.
+func (r *Router) Post(path string, h http.HandlerFunc) {
+	r.rtr.POST(r.prefix+path, handle(h))
+}
+
+// Put registers a new PUT route.
+func (r *Router) Put(path string, h http.HandlerFunc) {
+	r.rtr.PUT(r.prefix+path, handle(h))
+}
+
+// ServeHTTP implements http.Handler.
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.rtr.ServeHTTP(w, req)
+}
+
+// FileServe returns a new http.HandlerFunc that serves files from dir.
+// Using routes must provide the *filepath parameter.
+func FileServe(dir string) http.HandlerFunc {
+	fs := http.FileServer(http.Dir(dir))
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Path = Param(Context(r), "filepath")
+		fs.ServeHTTP(w, r)
+	}
+}
---
+++ router/router_test.go
@@ -0,0 +1,196 @@
+package router
+
+import (
+	"net/http"
+	"testing"
+)
+
+type mockResponseWriter struct{}
+
+func (m *mockResponseWriter) Header() (h http.Header) {
+	return http.Header{}
+}
+
+func (m *mockResponseWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func (m *mockResponseWriter) WriteString(s string) (n int, err error) {
+	return len(s), nil
+}
+
+func (m *mockResponseWriter) WriteHeader(int) {}
+
+type mockFileSystem struct {
+	opened bool
+}
+
+type handlerStruct struct {
+	handeled *bool
+}
+
+func TestRouter(t *testing.T) {
+	var get, head, options, post, put, patch, delete bool
+
+	router := New()
+	router.Get("/GET", func(w http.ResponseWriter, r *http.Request) {
+		get = true
+	})
+	router.Head("/GET", func(w http.ResponseWriter, r *http.Request) {
+		head = true
+	})
+	router.Options("/GET", func(w http.ResponseWriter, r *http.Request) {
+		options = true
+	})
+	router.Post("/POST", func(w http.ResponseWriter, r *http.Request) {
+		post = true
+	})
+	router.Put("/PUT", func(w http.ResponseWriter, r *http.Request) {
+		put = true
+	})
+	router.Patch("/PATCH", func(w http.ResponseWriter, r *http.Request) {
+		patch = true
+	})
+	router.Delete("/DELETE", func(w http.ResponseWriter, r *http.Request) {
+		delete = true
+	})
+
+	w := new(mockResponseWriter)
+
+	r, _ := http.NewRequest("GET", "/GET", nil)
+	router.ServeHTTP(w, r)
+	if !get {
+		t.Error("routing GET failed")
+	}
+
+	r, _ = http.NewRequest("HEAD", "/GET", nil)
+	router.ServeHTTP(w, r)
+	if !head {
+		t.Error("routing HEAD failed")
+	}
+
+	r, _ = http.NewRequest("OPTIONS", "/GET", nil)
+	router.ServeHTTP(w, r)
+	if !options {
+		t.Error("routing OPTIONS failed")
+	}
+
+	r, _ = http.NewRequest("POST", "/POST", nil)
+	router.ServeHTTP(w, r)
+	if !post {
+		t.Error("routing POST failed")
+	}
+
+	r, _ = http.NewRequest("PUT", "/PUT", nil)
+	router.ServeHTTP(w, r)
+	if !put {
+		t.Error("routing PUT failed")
+	}
+
+	r, _ = http.NewRequest("PATCH", "/PATCH", nil)
+	router.ServeHTTP(w, r)
+	if !patch {
+		t.Error("routing PATCH failed")
+	}
+
+	r, _ = http.NewRequest("DELETE", "/DELETE", nil)
+	router.ServeHTTP(w, r)
+	if !delete {
+		t.Error("routing DELETE failed")
+	}
+}
+
+func TestRouterWithPrefix(t *testing.T) {
+	var get, head, options, post, put, patch, delete bool
+
+	router := New()
+	router = router.WithPrefix("/api")
+	router.Get("/GET", func(w http.ResponseWriter, r *http.Request) {
+		get = true
+	})
+	router.Head("/GET", func(w http.ResponseWriter, r *http.Request) {
+		head = true
+	})
+	router.Options("/GET", func(w http.ResponseWriter, r *http.Request) {
+		options = true
+	})
+	router.Post("/POST", func(w http.ResponseWriter, r *http.Request) {
+		post = true
+	})
+	router.Put("/PUT", func(w http.ResponseWriter, r *http.Request) {
+		put = true
+	})
+	router.Patch("/PATCH", func(w http.ResponseWriter, r *http.Request) {
+		patch = true
+	})
+	router.Delete("/DELETE", func(w http.ResponseWriter, r *http.Request) {
+		delete = true
+	})
+
+	w := new(mockResponseWriter)
+
+	r, _ := http.NewRequest("GET", "/api/GET", nil)
+	router.ServeHTTP(w, r)
+	if !get {
+		t.Error("routing GET failed")
+	}
+
+	r, _ = http.NewRequest("HEAD", "/api/GET", nil)
+	router.ServeHTTP(w, r)
+	if !head {
+		t.Error("routing HEAD failed")
+	}
+
+	r, _ = http.NewRequest("OPTIONS", "/api/GET", nil)
+	router.ServeHTTP(w, r)
+	if !options {
+		t.Error("routing OPTIONS failed")
+	}
+
+	r, _ = http.NewRequest("POST", "/api/POST", nil)
+	router.ServeHTTP(w, r)
+	if !post {
+		t.Error("routing POST failed")
+	}
+
+	r, _ = http.NewRequest("PUT", "/api/PUT", nil)
+	router.ServeHTTP(w, r)
+	if !put {
+		t.Error("routing PUT failed")
+	}
+
+	r, _ = http.NewRequest("PATCH", "/api/PATCH", nil)
+	router.ServeHTTP(w, r)
+	if !patch {
+		t.Error("routing PATCH failed")
+	}
+
+	r, _ = http.NewRequest("DELETE", "/api/DELETE", nil)
+	router.ServeHTTP(w, r)
+	if !delete {
+		t.Error("routing DELETE failed")
+	}
+}
+
+func TestRouterParam(t *testing.T) {
+	var param bool
+
+	router := New()
+	router.Get("/user/:username", func(w http.ResponseWriter, r *http.Request) {
+		param = true
+
+		ctx := Context(r)
+		width := Param(ctx, "username")
+		if width != "foo" {
+			t.Fatal("Got no param!")
+		}
+	})
+
+	w := new(mockResponseWriter)
+
+	r, _ := http.NewRequest("GET", "/user/foo", nil)
+	router.ServeHTTP(w, r)
+	if !param {
+		t.Error(param)
+	}
+}
